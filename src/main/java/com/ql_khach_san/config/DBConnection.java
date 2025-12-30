7f5bbf08cf0df917241919dd6399091423e60d55
Hoang_Anh <kbhoanganh05@gmail.com>
2025-12-30 14:06:48 +0700

    demo

--- src/main/java/com/ql_khach_san/config/DBConnection.java
+++ src/main/java/com/ql_khach_san/config/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
     
     private static final String URL = "jdbc:mysql://localhost:3306/db_ql_khach_san?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "123";
     
     private static Connection connection;
     
